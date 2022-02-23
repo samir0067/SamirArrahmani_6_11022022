51bf595bda8dbd62bd1f451343346ee68c6c1e25
samir <arrahmani.samir@gmail.com>
2022-02-23 09:50:47 +0100

    refactor and rollback

--- src/javascript/pages/index.js
+++ src/javascript/pages/index.js
@@ -1,19 +1,25 @@
+const header = document.querySelector(".header")
 const main = document.querySelector(".main")
+
+header.innerHTML =
+  `<a href="../../../index.html">
+      <img class="header_logo" src="./src/assets/images/logo.svg" alt="image du logo fisheye"/>
+    </a>
+    <h1 class="header_title">Nos Photographes</h1>`
 
 const viewPhotographers = (photographersTable) => {
   photographersTable.forEach((photographe) => {
     main.innerHTML +=
       `<div class="card">
-        <a role="button" class="card_buttonContent"  href="./photographer.html?id=${photographe.id}&name=${photographe.name}">
-          <img src="./src/assets/photographers/Photographers ID Photos/${photographe.portrait}" alt="portrait ${photographe.name}" />
+        <a role="button" class="card_buttonContent"  href="./photographer.html?nom=${photographe.name}&identifiant=${photographe.id}">
+          <img src="./src/assets/photographers/Photographers ID Photos/${photographe.portrait}" alt="portrait de ${photographe.name}" />
           <h2 class="card_title">${photographe.name}</h2>
         </a>
         <div class="card_info">
           <h3 class="card_info_title">${photographe.city}, ${photographe.country}</h3>
           <p class="card_info_subTitle"> ${photographe.tagline}</p>
-          <p class="card_info_prix" aria-label="${photographe.price}€ par jour">${photographe.price}€/jour</p>
+          <p class="card_info_price" aria-label="${photographe.price}€ par jour">${photographe.price}€/jour</p>
         </div>
-      </div>
-      `
+      </div>`
   })
 }
--- src/javascript/utils/fetchData.js
+++ src/javascript/utils/fetchData.js
@@ -1,9 +1,11 @@
 const fetchData = async() => {
   return await fetch("photographers.json")
     .then((response) => response.json())
-    .then((data) => {
-      let photographes = data.photographers
-      viewPhotographers(photographes)
-    })
 }
 fetchData()
+  .then(data => {
+    console.log('données récupérées =>', data)
+    let photographes = data.photographers
+    viewPhotographers(photographes)
+  })
+  .catch(erreur => console.log("Erreur dans la récupération des données =>", erreur.message))
